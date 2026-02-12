4a628bb4a52f838a7ab96e39105f7e9c364df3f9
minhtungmobile-maker <minhtungmobile@gmail.com>
2026-02-13 01:24:05 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+﻿document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Cảm ơn bạn! Chúng tôi sẽ liên hệ ngay.");
+});
---
+++ sw.js
@@ -0,0 +1,12 @@
+﻿const CACHE_NAME="v6-cache";
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache=>cache.addAll(["./","./index.html"]))
+);
+});
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(r=>r||fetch(e.request))
+);
+});
